4dfc5b3667edfa94459ee342e182170acb5efb2c
mailorcanto <130517407+mailorcanto@users.noreply.github.com>
2023-05-10 20:08:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -156,6 +156,25 @@ console.log(tartaruga) //vai imprimir o objeto tartaruga com todas as suas propr
 //b) O que faz a sintaxe dos três pontos antes do nome de um objeto? = Faz uma cópia daquele objeto
 */
 
+/*
+//3. Leia o código abaixo
+function minhaFuncao(objeto, propriedade) {
+	return objeto[propriedade]
+}
+
+const pessoa = {
+  nome: "Caio", 
+  idade: 23, 
+  backender: false
+}
+
+console.log(minhaFuncao(pessoa, "backender")) 
+console.log(minhaFuncao(pessoa, "altura"))
+
+//a) O que vai ser impresso no console? = false para o primeiro e undefined para o segundo
+
+//b) Explique o valor impresso no console. Você sabe por que isso aconteceu? = pois backender retorna falso e não existe propriedade altura no objeto 'pessoa'
+*/
 
 //Exercícios de escrita de código
 
